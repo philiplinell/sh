f24ee02b2ff4b4624689ed01736562e97b26c79c
Philip Linell <linell@hey.com>
2022-12-03 12:12:11 +0100

    Update linter settings
    Add config file.

--- sh.go
+++ sh.go
@@ -2,6 +2,8 @@ package sh
 
 import "log"
 
+// FailOnErr will log with a log.Fatal if there is any errors (meaning it will
+// directly exit the routine with a exit code 1).
 func FailOnErr(err error) {
 	if err != nil {
 		log.Fatal(err)
